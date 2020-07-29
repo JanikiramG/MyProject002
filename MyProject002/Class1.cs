c24f3be5118fe191c8d07b6f56008f0d7599e54b
janakiram775 <janakiram775@gmail.com>
2020-07-29 09:24:17 +0530

    Add project files.

---
+++ MyProject002/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyProject002
+{
+	public class Class1
+	{
+	}
+}
---
+++ MyProject002/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject002
+{
+	class MyClass
+	{
+		int a = 10;
+		//12
+	}
+}
